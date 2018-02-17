0ae873860cef12eab66aacb9ccd6285c7fe78eb6
Mariana Souza <marianacaldassouza@gmail.com>
2018-02-16 19:02:51 -0800

    human resources program with the object Employee as functions exported as declared

---
+++ workAround-object-importsStatements.js
@@ -0,0 +1,20 @@
+/* In this exercice, we are importing the functions as one object and variables with their real names.
+The property salary is used as Employee.salary since it is inside of the object Employee
+*/
+
+import {getCadre, calculateTax, getBenefits, calculateBonus, reimbursementEligibility} from './employee';
+
+import Employee from './employee-object-exportsStatements';
+
+function getEmployeeInformation(inputSalary) {
+  Employee.salary = inputSalary;
+  console.log('Cadre: ' + getCadre());
+  console.log('Tax: ' + calculateTax());
+  console.log('Benefits: ' + getBenefits());
+  console.log('Bonus: ' + calculateBonus());
+  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+}
+
+getEmployeeInformation(35000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
